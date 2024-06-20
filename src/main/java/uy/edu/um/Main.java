084ad65bdd1ef1aa4b20febce29a0bd8b981a34e
JoacoGil <joaquingil2005@gmail.com>
2024-06-20 00:58:22 -0300

    (Arreglo comentario de commit anterior) Agrego DataStorage y funcion para get del top10songs. Conecto el main con el menu. Problemas al cargar archivo csv.

--- src/main/java/uy/edu/um/Main.java
+++ src/main/java/uy/edu/um/Main.java
@@ -7,6 +7,7 @@ import uy.edu.um.entities.Menu;
 public class Main {
     public static void main(String[] args) {
         CargaDatos cargaDatos = new CargaDatos();
+        //problemas aca
         MyList<EntradaTop50> entradasTop50 = cargaDatos.cargarDatosTop50("./reduced_data.csv");
         Menu.Menu();
     }
